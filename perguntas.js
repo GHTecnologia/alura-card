1e25a1b799404e959c2ba43794c4fb3245b82bb1
GHTecnologia <00001093573557SP@al.educacao.sp.gov.br>
2024-11-06 08:18:01 -0300

    gh

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Natureza',
+    'O que é natureza?',
+    'O Python é uma linguagem de programação' a natureza e um termo que se refere ao mundo Fisico ou seja a tudo o que e possivel VTTRegion, tocar, sentir ou Medir
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
